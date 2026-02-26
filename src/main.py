8a8022a7c891927ee1303703aa6c7fd4e4e1e315
Md-Asif-Hasan <asifhasan10122000@gmail.com>
2026-02-27 00:27:21 +0600

    basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
+import utils
 from datetime import datetime
 
 name = input("Enter your name: ")
 
 print(f"Your name is: {name}")
 print(f"Current time is: {datetime.now()}")
+
+print(utils.add(5, 5)) 
+print(utils.sub(10, 5))
